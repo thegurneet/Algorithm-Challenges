582d598c66fbfb815fe95de6bcb84fcb75faf119
Singh, Gurneet (Capgemini Flex) <gurneet.singh@capgemini.com>
2019-10-08 10:00:38 +0100

    update

--- DiagonalDifference/app.js
+++ DiagonalDifference/app.js
@@ -2,21 +2,55 @@
 /*
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+7 8 9
+
+ a = (1 + 5 + 9) - (3 + 5 + 7)
 */
 
 
 function printThis(a){
-
+    var b = ''
     for(var i = 0; i < a.length; i++){
-        var b = ' '
         for(var x = 0; x < a[i].length; x++){
-            b.concat(a[i][x]);
-            console.log(b)
+          b =   b.concat(a[i][x] + ' ');
         }
-
+        console.log(b)
+        b = '';
     }
+
+    DiagonalDifference(a)
+}
+
+
+function DiagonalDifference(a){
+    var h = 0;
+    var p = 0;
+    var left = 0
+    var right = 0;
+
+    for (let i = 0; i < a.length; i++){
+        for(let y = 0; y < a[i].length; y++){
+            if(y == h){
+                left += a[i][y]
+            }
+        }
+        h++;
+    }
+
+    for(let i = a.length ; i > 0 ; i--){
+        for(let y = 0; y < a[i-1].length; y++){
+            if(y == p){
+                right += a[i-1][y]
+            }
+        }
+        p++;
+    }
+    console.log(left - right)
 
 }
 
 
-printThis([[1,2,3],[4,5,6],[7,8,9]])
+printThis([[3,2,3,5],[4,5,6,6],[7,8,9,8],[5,2,6,4]])
